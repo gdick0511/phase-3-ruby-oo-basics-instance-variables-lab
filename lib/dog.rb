2657cf6bbfe7b4fa6599e760e30fa161eefd80aa
garrettdick <garrettd0511@gmail.com>
2021-11-02 09:14:02 -0500

    commit tree badacffec90f9ed3ef8455480df1ccc9226bd39e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+    def name=(dog_name)
+        @this_dogs_name = dog_name # this is 'setting' the dogs name
+    end
 
+    def name
+        @this_dogs_name # this is 'getting' the dogs name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+lassie.name #=> "Lassie"
